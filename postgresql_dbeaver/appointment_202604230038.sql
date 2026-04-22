5bac0fbf3d25eee8c615df37eada9e0c98b7af33
AlexanderSYN <000777000casa@gmail.com>
2026-04-23 00:57:02 +0300

    added sql files (for dbeaver) postgresql

---
+++ postgresql_dbeaver/appointment_202604230038.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.appointment (type_pension,recipients,"size",start_date) VALUES
+	 (1,1,6000,'2026-04-23'),
+	 (1,2,16000,'2026-04-23'),
+	 (2,3,18000,'2026-04-08'),
+	 (3,4,16288,'2026-04-22'),
+	 (4,5,16288,'2026-04-15');
